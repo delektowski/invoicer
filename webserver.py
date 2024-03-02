e5c8bfb7ec0e81b6151d51119635ef374ffce9fe
Marcin Delektowski <marcin.delektowski@gmail.com>
2024-03-02 19:29:38 +0100

    add jinja refactor

--- webserver.py
+++ webserver.py
@@ -19,10 +19,14 @@ templates = Jinja2Templates(directory="templates")
 
 handle_table_creation()
 
+
 @app.get("/")
 async def send_invoice_webpage(request: Request):
     latest_invoice = get_invoice()
-    return templates.TemplateResponse(request=request, name="invoice_form.html", context=latest_invoice)
+    return templates.TemplateResponse(
+        request=request, name="invoice_form.html", context=latest_invoice
+    )
+
 
 @app.get("/invoice")
 async def send_invoice_webpage(request: Request):
@@ -42,10 +46,129 @@ async def send_invoice_webpage(request: Request):
     invoice_unit_measure = request.query_params.get("invoice_unit_measure")
     invoice_hour_rates = request.query_params.get("invoice_hour_rates")
     invoice_hours_number = request.query_params.get("invoice_hours_number")
-    
-    invoice_counter = InvoiceCounter(invoice_hour_rates,invoice_hours_number)
-    print("KOKOKO", float(invoice_counter.get_brutto_value()))
-    print("IOIOIOOI", number_to_word(float(invoice_counter.get_brutto_value())))
+
+    invoice_counter = InvoiceCounter(invoice_hour_rates, invoice_hours_number)
+    # invoice_dict = {
+    #     "invoice_number": invoice_number,
+    #     "invoice_date": invoice_date,
+    #     "invoice_pay_date": invoice_pay_date,
+    #     "invoice_pay_type": invoice_pay_type,
+    #     "invoice_account_number": invoice_account_number,
+    #     "invoice_seller_name": invoice_seller_name,
+    #     "invoice_seller_address": invoice_seller_address,
+    #     "invoice_seller_nip": invoice_seller_nip,
+    #     "invoice_buyer_name": invoice_buyer_name,
+    #     "invoice_buyer_address": invoice_buyer_address,
+    #     "invoice_buyer_nip": invoice_buyer_nip,
+    #     "invoice_specification": invoice_specification,
+    #     "invoice_classification": invoice_classification,
+    #     "invoice_unit_measure": invoice_unit_measure,
+    #     "invoice_hour_rates": invoice_hour_rates,
+    #     "invoice_hours_number": invoice_hours_number,
+    #     "invoice_netto_value": invoice_counter.get_netto_value(),
+    #     "invoice_vat_value": invoice_counter.get_vat_value(),
+    #     "invoice_brutto_value": invoice_counter.get_brutto_value(),
+    #     "invoice_value_in_words": number_to_word(float(invoice_counter.get_brutto_value())),
+
+    # }
+
+    invoice_dict = {
+        "invoice_top_section": {
+            "invoice_number": (invoice_number, "invoice_number"),
+            "invoice_date": (invoice_date, "invoice_date"),
+            "invoice_pay_date": (invoice_pay_date, "invoice_pay_date"),
+            "invoice_pay_type": (invoice_pay_type, "invoice_pay_type"),
+            "invoice_account_number": (
+                invoice_account_number,
+                "invoice_account_number",
+            ),
+        },
+        "invoice_middle_section": {
+            "invoice_seller_name": (invoice_seller_name, "invoice_seller_name"),
+            "invoice_seller_address": (
+                invoice_seller_address,
+                "invoice_seller_address",
+            ),
+            "invoice_seller_nip": (invoice_seller_nip, "invoice_seller_nip"),
+            "invoice_buyer_name": (invoice_buyer_name, "invoice_buyer_name"),
+            "invoice_buyer_address": (invoice_buyer_address, "invoice_buyer_address"),
+            "invoice_buyer_nip": (invoice_buyer_nip, "invoice_buyer_nip"),
+        },
+        "invoice_horizontal_section": {
+            "invoice_specification": (
+                invoice_specification,
+                "invoice_specification",
+                "specification",
+            ),
+            "invoice_classification": (
+                invoice_classification,
+                "invoice_classification",
+                "classification",
+            ),
+            "invoice_unit_measure": (
+                invoice_unit_measure,
+                "invoice_unit_measure",
+                "unit-measure",
+            ),
+            "invoice_hour_rates": (
+                invoice_hour_rates,
+                "invoice_hour_rates",
+                "hour-rates",
+            ),
+            "invoice_hours_number": (
+                invoice_hours_number,
+                "invoice_hours_number",
+                "hours-number",
+            ),
+        },
+        "invoice_bottom_section": {
+            "invoice_netto_value": (
+                invoice_counter.get_netto_value(),
+                "invoice_netto_value",
+            ),
+            "invoice_vat_value": (invoice_counter.get_vat_value(), "invoice_vat_value"),
+            "invoice_brutto_value": (
+                invoice_counter.get_brutto_value(),
+                "invoice_brutto_value",
+            ),
+            "invoice_currency": (
+                "PLN",
+                "invoice_currency",
+            ),
+            "invoice_value_in_words": (
+                number_to_word(float(invoice_counter.get_brutto_value())),
+                "invoice_value_in_words",
+            ),
+        },
+    }
+
+    return templates.TemplateResponse(
+        request=request,
+        name="invoice_page.jinja",
+        context={"invoice_dict": invoice_dict},
+    )
+
+
+@app.get("/invoice-pdf")
+async def send_invoice_webpage(request: Request):
+    invoice_number = request.query_params.get("invoice_number")
+    invoice_date = request.query_params.get("invoice_date")
+    invoice_pay_date = request.query_params.get("invoice_pay_date")
+    invoice_pay_type = request.query_params.get("invoice_pay_type")
+    invoice_account_number = request.query_params.get("invoice_account_number")
+    invoice_seller_name = request.query_params.get("invoice_seller_name")
+    invoice_seller_address = request.query_params.get("invoice_seller_address")
+    invoice_seller_nip = request.query_params.get("invoice_seller_nip")
+    invoice_buyer_name = request.query_params.get("invoice_buyer_name")
+    invoice_buyer_address = request.query_params.get("invoice_buyer_address")
+    invoice_buyer_nip = request.query_params.get("invoice_buyer_nip")
+    invoice_specification = request.query_params.get("invoice_specification")
+    invoice_classification = request.query_params.get("invoice_classification")
+    invoice_unit_measure = request.query_params.get("invoice_unit_measure")
+    invoice_hour_rates = request.query_params.get("invoice_hour_rates")
+    invoice_hours_number = request.query_params.get("invoice_hours_number")
+
+    invoice_counter = InvoiceCounter(invoice_hour_rates, invoice_hours_number)
     invoice_dict = {
         "invoice_number": invoice_number,
         "invoice_date": invoice_date,
@@ -68,27 +191,27 @@ async def send_invoice_webpage(request: Request):
         "invoice_netto_value": invoice_counter.get_netto_value(),
         "invoice_vat_value": invoice_counter.get_vat_value(),
         "invoice_brutto_value": invoice_counter.get_brutto_value(),
-        "invoice_value_in_words": number_to_word(float(invoice_counter.get_brutto_value())),
-   
+        "invoice_value_in_words": number_to_word(
+            float(invoice_counter.get_brutto_value())
+        ),
     }
- 
+
     return templates.TemplateResponse(
-        request=request,
-        name="invoice_page.html",
-        context=invoice_dict
-       
+        request=request, name="invoice_pdf.html", context=invoice_dict
     )
+
 
 @app.get("/file")
 def send_file(request: Request):
-    invoice_file_path ="./output3.pdf"
+    invoice_file_path = "./output3.pdf"
     custom_filename = "output3.pdf"
 
-  
     return FileResponse(invoice_file_path, status_code=200, filename=custom_filename)
 
+
 @app.post("/form", status_code=201)
-def get_form(request: Request,
+def get_form(
+    request: Request,
     invoice_number: Annotated[str, Form()],
     invoice_date: Annotated[str, Form()],
     invoice_pay_date: Annotated[str, Form()],
@@ -106,31 +229,8 @@ def get_form(request: Request,
     invoice_hour_rates: Annotated[str, Form()],
     invoice_hours_number: Annotated[str, Form()],
 ):
-    
+
     invoice_dict = {
-            "invoice_number": invoice_number,
-            "invoice_date": invoice_date,
-            "invoice_pay_date": invoice_pay_date,
-            "invoice_pay_type": invoice_pay_type,
-            "invoice_account_number": invoice_account_number,
-            "invoice_seller_name": invoice_seller_name,
-            "invoice_seller_address": invoice_seller_address,
-            "invoice_seller_nip": invoice_seller_nip,
-            "invoice_buyer_name": invoice_buyer_name,
-            "invoice_buyer_address": invoice_buyer_address,
-            "invoice_buyer_nip": invoice_buyer_nip,
-            "invoice_specification": invoice_specification,
-            "invoice_classification": invoice_classification,
-            "invoice_unit_measure": invoice_unit_measure,
-            "invoice_unit_measure": invoice_unit_measure,
-            "invoice_unit_measure": invoice_unit_measure,
-            "invoice_hour_rates": invoice_hour_rates,
-            "invoice_hours_number": invoice_hours_number,
-        }
-    
-    redirect_url = "/invoice?" + "&".join(
-    f"{key}={value}"
-    for key, value in {
         "invoice_number": invoice_number,
         "invoice_date": invoice_date,
         "invoice_pay_date": invoice_pay_date,
@@ -149,13 +249,60 @@ def get_form(request: Request,
         "invoice_unit_measure": invoice_unit_measure,
         "invoice_hour_rates": invoice_hour_rates,
         "invoice_hours_number": invoice_hours_number,
-    }.items()
+    }
+
+    redirect_url = "/invoice?" + "&".join(
+        f"{key}={value}"
+        for key, value in {
+            "invoice_number": invoice_number,
+            "invoice_date": invoice_date,
+            "invoice_pay_date": invoice_pay_date,
+            "invoice_pay_type": invoice_pay_type,
+            "invoice_account_number": invoice_account_number,
+            "invoice_seller_name": invoice_seller_name,
+            "invoice_seller_address": invoice_seller_address,
+            "invoice_seller_nip": invoice_seller_nip,
+            "invoice_buyer_name": invoice_buyer_name,
+            "invoice_buyer_address": invoice_buyer_address,
+            "invoice_buyer_nip": invoice_buyer_nip,
+            "invoice_specification": invoice_specification,
+            "invoice_classification": invoice_classification,
+            "invoice_unit_measure": invoice_unit_measure,
+            "invoice_unit_measure": invoice_unit_measure,
+            "invoice_unit_measure": invoice_unit_measure,
+            "invoice_hour_rates": invoice_hour_rates,
+            "invoice_hours_number": invoice_hours_number,
+        }.items()
+    )
+
+    url_pdf = "invoice-pdf?" + "&".join(
+        f"{key}={value}"
+        for key, value in {
+            "invoice_number": invoice_number,
+            "invoice_date": invoice_date,
+            "invoice_pay_date": invoice_pay_date,
+            "invoice_pay_type": invoice_pay_type,
+            "invoice_account_number": invoice_account_number,
+            "invoice_seller_name": invoice_seller_name,
+            "invoice_seller_address": invoice_seller_address,
+            "invoice_seller_nip": invoice_seller_nip,
+            "invoice_buyer_name": invoice_buyer_name,
+            "invoice_buyer_address": invoice_buyer_address,
+            "invoice_buyer_nip": invoice_buyer_nip,
+            "invoice_specification": invoice_specification,
+            "invoice_classification": invoice_classification,
+            "invoice_unit_measure": invoice_unit_measure,
+            "invoice_unit_measure": invoice_unit_measure,
+            "invoice_unit_measure": invoice_unit_measure,
+            "invoice_hour_rates": invoice_hour_rates,
+            "invoice_hours_number": invoice_hours_number,
+        }.items()
     )
 
     insert_invoice(table_name="invoices", data=tuple(invoice_dict.values()))
-    pdf_creator = PdfCreator(str(request.base_url))
+    pdf_creator = PdfCreator(str(request.base_url) + url_pdf)
     pdf_creator.create_pdf()
-    
+
     response = RedirectResponse(url=redirect_url, status_code=301)
 
     return response
